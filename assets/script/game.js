a9dc747cfd208524daa87409409e013c214296e5
Jack Walker <mba_jack@MacBook-Pro.attlocal.net>
2016-05-13 19:05:47 -0500

    initial site files

---
+++ assets/script/game.js
@@ -0,0 +1,22 @@
+// RPG Week 4
+
+
+/*
+create 4 divs that will hold images
+-> animate to various positions on the screen
+
+Game
+
+var healthPoints;
+var attackPower;
+var counterAttackPower;
+
+These are different for each monster.
+Create functions for each possibility.  Store function in variable with monster name.
+Get monster name when selected
+
+var baseAttackPower;
+var baseHealthPoints;
+var baseCounterAttackPower;
+
+*/
